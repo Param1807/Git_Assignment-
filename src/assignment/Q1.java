5bad9990df89ecb19c9e5183fdcaa2ca151e82c7
Paramjeet Solanki <paramjeet.solanki@nagarro.com>
2022-12-28 16:23:17 +0530

    First Commit

---
+++ src/assignment/Q1.java
@@ -0,0 +1,16 @@
+package assignment;
+
+public class Q1 {
+	
+	public static void main(String[] args) {
+
+		 
+
+        String str ="This   is    a   practice     question";
+        String noSpaceStr = str.replaceAll("\\s+"," ");  
+        System.out.println(noSpaceStr);
+
+ 
+
+    }
+}
